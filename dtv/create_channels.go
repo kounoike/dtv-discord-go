1826ec92fbdd5baa3e8c4ebefec26bda3ec91858
KOUNOIKE Yuusuke <kounoike.yuusuke@gmail.com>
2023-03-25 00:39:39 +0900

    番組情報の詳細化

--- dtv/create_channels.go
+++ dtv/create_channels.go
@@ -1,17 +1,26 @@
 package dtv
 
+import "golang.org/x/exp/slog"
+
 func (dtv *DTVUsecase) CreateChannels() error {
 	services, err := dtv.mirakc.ListServices()
+	if err != nil {
+		return err
+	}
+	slog.Debug("ListServices OK", "len(services)", len(services))
+
+	err = dtv.discord.UpdateChannelsCache()
 	if err != nil {
 		return err
 	}
 
 	for _, service := range services {
 		// 無ければ作ってくれるし、キャッシュにも入る
-		_, err := dtv.discord.GetChannelID("録画-番組情報", service.Name)
+		ch, err := dtv.discord.GetCachedChannel("録画-番組情報", service.Name)
 		if err != nil {
 			return err
 		}
+		slog.Debug("GetCachedChannel", "ch.ID", ch.ID, "ch.Name", ch.Name)
 	}
 	return nil
 }
--- template/program_message.go
+++ template/program_message.go
@@ -2,8 +2,12 @@ package template
 
 import (
 	"bytes"
+	"encoding/json"
+	"fmt"
 	"text/template"
+	"time"
 
+	jsoniter "github.com/json-iterator/go"
 	"github.com/kounoike/dtv-discord-go/db"
 	"github.com/kounoike/dtv-discord-go/mirakc/mirakc_model"
 )
@@ -17,10 +21,44 @@ var programMessageTemplateString = `============================================
 {{ .Program.Name }}
 {{ .Program.Description }}
 {{ .Service.Name }}
+{{ .Program.Json | toExtendStr }}
+{{ .Program.StartAt |toTimeStr }}～{{ .Program.Duration | toDurationStr }}
 `
 
+//{{ .Program.StartTime.Format "2006/01/02 03:04"}}～{{ .Program.EndTime.Format "2006/01/02 03:04"}}
+
+func toTimeStr(t int64) string {
+	return time.Unix(t/1000, (t%1000)*1000).Format("2006/01/02 03:04")
+}
+
+func toDurationStr(d int32) string {
+	hour := d / (60 * 60 * 1000)
+	hourStr := ""
+	if hour > 0 {
+		hourStr = fmt.Sprintf("%d時間", hour)
+	}
+	min := d % (60 * 60 * 1000) / (60 * 1000)
+	minStr := fmt.Sprintf("%d分", min)
+	return hourStr + minStr
+}
+
+func toExtendStr(j json.RawMessage) string {
+	b, _ := j.MarshalJSON()
+	any := jsoniter.Get(b, "extended")
+	str := ""
+	for _, key := range any.Keys() {
+		str += fmt.Sprintf("%s:%s", key, any.Get(key).ToString())
+	}
+	return str
+}
+
 func GetProgramMessage(program db.Program, service mirakc_model.Service) (string, error) {
-	tmpl, err := template.New("program-message").Parse(programMessageTemplateString)
+	funcMap := map[string]interface{}{
+		"toTimeStr":     toTimeStr,
+		"toDurationStr": toDurationStr,
+		"toExtendStr":   toExtendStr,
+	}
+	tmpl, err := template.New("program-message").Funcs(funcMap).Parse(programMessageTemplateString)
 	if err != nil {
 		return "", err
 	}
