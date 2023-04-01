162a0814ceb3cfef51083d8050e8f847fc260a11
KOUNOIKE Yuusuke <kounoike.yuusuke@gmail.com>
2023-04-01 23:01:14 +0900

    fix typo

--- dtv/check_failed_task.go
+++ dtv/check_failed_task.go
@@ -14,7 +14,7 @@ import (
 )
 
 func (dtv *DTVUsecase) CheckFailedTask(ctx context.Context) error {
-	dtv.logger.Debug("Start CheckCompletedTask")
+	dtv.logger.Debug("Start CheckFailedTask")
 	if dtv.inspector == nil {
 		return nil
 	}
