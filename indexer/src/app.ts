c2334dfe87d206a80fd2999a3a39a83dcb0530ca
KOUNOIKE Yuusuke <kounoike.yuusuke@gmail.com>
2023-04-15 14:23:17 +0000

    正規化して比較

--- indexer/src/app.ts
+++ indexer/src/app.ts
@@ -4,10 +4,23 @@ import mysql, { RowDataPacket } from "mysql2/promise"
 import Fuse from "fuse.js"
 import fs from "fs"
 
-interface Program extends RowDataPacket {
+interface Program {
   id: number
   name: string
   description: string
+  startAt: Date
+  channel: string
+  extended: string
+}
+
+interface IProgram extends RowDataPacket {
+  id: number
+  name: string
+  description: string
+  start_at: number
+  channel: string
+  message_id: string
+  json: string
 }
 
 async function main() {
@@ -18,8 +31,32 @@ async function main() {
     database: "dtv",
   })
 
-  const [rows, fields] = await connection.query<Program[]>({
-    sql: "SELECT * FROM program",
+  const [rows, fields] = await connection.query<IProgram[]>({
+    sql: `
+    SELECT
+      program.*,
+      service.name AS channel,
+      program_message.message_id
+    FROM program
+      JOIN service ON program.service_id = service.service_id
+        AND program.network_id = service.service_id
+      JOIN program_message ON program.id = program_message.program_id
+      `,
+  })
+
+  const parseExtended = (extended: Object) => {
+    return JSON.stringify(extended)
+  }
+
+  const document = rows.map((row): Program => {
+    return {
+      id: row.id,
+      name: row.name.normalize(),
+      description: row.description.normalize(),
+      channel: row.channel.normalize(),
+      startAt: new Date(row.start_at),
+      extended: parseExtended(JSON.parse(row.json).extended).normalize(),
+    }
   })
 
   const options: Fuse.IFuseOptions<Program> = {
@@ -29,11 +66,11 @@ async function main() {
     shouldSort: true,
   }
 
-  const index = Fuse.createIndex(options.keys ?? [], rows)
-  fs.writeFileSync("document.json", JSON.stringify(rows))
+  const index = Fuse.createIndex(options.keys ?? [], document)
+  fs.writeFileSync("document.json", JSON.stringify(document))
   fs.writeFileSync("index.json", JSON.stringify(index.toJSON()))
 
-  const fuse = new Fuse(rows, options, index)
+  const fuse = new Fuse<Program>(document, options, index)
 
   console.log(fuse.search("トニカク").map((e) => [e.item.name, e.score]))
 
