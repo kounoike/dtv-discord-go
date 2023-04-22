8f6edb118fecb24caf7953edf8a245bcae9bd74a
KOUNOIKE Yuusuke <kounoike.yuusuke@gmail.com>
2023-04-22 11:16:22 +0000

    Revert "workerの設定を細分化"
    This reverts commit 4f8af6ba588aad9c30390112ec332972db638333.

--- docker-compose/worker/run_whisper.py
+++ docker-compose/worker/run_whisper.py
@@ -10,7 +10,7 @@ initial_prompt = """そうだ。今日はピクニックしない？天気もい
 model_size = sys.argv[1]
 
 # Run on GPU with FP16
-model = WhisperModel(model_size, device="cpu", compute_type="int8")
+model = WhisperModel(model_size, device="cuda", compute_type="float16")
 
 # or run on GPU with INT8
 # model = WhisperModel(model_size, device="cuda", compute_type="int8_float16")
